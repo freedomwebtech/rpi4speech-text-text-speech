764be73e463a4a48714b924c2480e2a5df102da5
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2022-10-27 13:50:22 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+sudo pip3 install gtts
+sudo apt -y install mpg321
+sudo pip3 install wikipedia
+sudo apt -y install flac
+sudo apt  -y install python3-pyaudio
+sudo pip3 install speechrecognition
+
+
